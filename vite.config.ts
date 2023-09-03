c6feee36a3e7c5d791a172f137f3578f0f8959ec
kyzinatra <danilsolom@mail.ru>
2023-09-03 20:59:01 +0300

    fix paths in vite

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	build: { target: "esnext" },
+	experimental: {
+		renderBuiltUrl(filename) {
+			return "./" + filename;
+		},
+	},
+});
